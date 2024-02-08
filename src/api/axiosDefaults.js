4aac3d62caf75af0300b2bbd602c96986f35f512
sam2829 scarisbrick <samscarisbrick@outlook.com>
2024-02-08 16:41:11 +0000

    Install axios and start building the sign up form

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://crf-api-1-952b00a16d56.herokuapp.com/';
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
