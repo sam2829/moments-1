edbe874d696e099245d84631efaaf9d45ac3aad8
sam2829 scarisbrick <samscarisbrick@outlook.com>
2024-02-09 18:20:00 +0000

    Add interceptors to refresh our tokens

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = 'https://crf-api-1-952b00a16d56.herokuapp.com/';
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
