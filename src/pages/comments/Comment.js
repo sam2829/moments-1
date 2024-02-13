447fb86b4c129e9385939c9018947edaab7b3d05
sam2829 scarisbrick <samscarisbrick@outlook.com>
2024-02-13 13:50:08 +0000

    Add edit comments

--- src/pages/comments/Comment.js
+++ src/pages/comments/Comment.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from "../../styles/Comment.module.css";
 import { Media } from 'react-bootstrap';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
@@ -6,6 +6,7 @@ import Avatar from '../../components/Avatar';
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import { MoreDropdown } from '../../components/MoreDropdown';
 import { axiosRes } from '../../api/axiosDefaults';
+import CommentEditForm from "./CommentEditForm";
 
 const Comment = (props) => {
   const { 
@@ -18,6 +19,9 @@ const Comment = (props) => {
     setPost,
     setComments 
   } = props;
+
+  const [showEditForm, setShowEditForm] = useState(false);
+
 
   const handleDelete = async () => {
     try {
@@ -42,23 +46,36 @@ const Comment = (props) => {
   const is_owner = currentUser?.username === owner;
 
   return (
-    <div>
-        <hr/>
-        <Media>
-            <Link to={`/profiles/${profile_id}`}>
-              <Avatar src={profile_image} />
-            </Link>
-            <Media.Body className='align-self-center ml-2' >
-              <span className={styles.Owner}>{owner}</span>
-              <span className={styles.Date}>{updated_at}</span>
-              <p>{content}</p>
-            </Media.Body>
-            {is_owner && (
-                <MoreDropdown handleEdit={() => {}} handleDelete={handleDelete} />
-            )}
-        </Media>
-    </div>
-  )
+    <>
+      <hr />
+      <Media>
+        <Link to={`/profiles/${profile_id}`}>
+          <Avatar src={profile_image} />
+        </Link>
+        <Media.Body className="align-self-center ml-2">
+          <span className={styles.Owner}>{owner}</span>
+          <span className={styles.Date}>{updated_at}</span>
+          {showEditForm ? (
+            <CommentEditForm 
+              id={id}
+              profile_id={profile_id}
+              content={content}
+              profileImage={profile_image}
+              setComments={setComments}
+              setShowEditForm={setShowEditForm} />
+          ) : (
+            <p>{content}</p>
+          )}
+        </Media.Body>
+        {is_owner && !showEditForm && (
+          <MoreDropdown
+            handleEdit={() => setShowEditForm(true)}
+            handleDelete={handleDelete}
+          />
+        )}
+      </Media>
+    </>
+  );
 }
 
 export default Comment
