87ac3b59ec675f7d83058c26debddef2e52a37b4
sam2829 scarisbrick <samscarisbrick@outlook.com>
2024-02-15 14:26:07 +0000

    Set up project ready for tests

---
+++ src/mocks/handlers.js
@@ -0,0 +1,20 @@
+const baseURL = "https://crf-api-1-952b00a16d56.herokuapp.com/"
+
+export const handlers = [
+    rest.get(`${base.URL}dj-rest-auth/user/`, (req, res, ctx) => {
+        return res(
+            ctx.json({
+                "pk": 9,
+                "username": "harry28",
+                "email": "",
+                "first_name": "",
+                "last_name": "",
+                "profile_id": 9,
+                "profile_image": "https://res.cloudinary.com/dc8vjzinx/image/upload/v1/media/images/mj11_yzyqb9"
+            })
+        );
+    }),
+    rest.post(`${baseURL}dj-rest-auth/logout/`, (req, res, ctx) => {
+        return res(ctx.status(200));
+    }),
+];
