b0c8307137ae4cc41adbda4bf07203ca8f5e8c8e
sam2829 scarisbrick <samscarisbrick@outlook.com>
2024-02-16 10:36:11 +0000

    add correct Navbar import from react bootstrap in navbar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "react-bootstrap";
+import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import logo from "../assets/logo.png";
