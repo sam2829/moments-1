b098d813ee4860772068d51ddd7897d674bb5cd6
sam2829 scarisbrick <samscarisbrick@outlook.com>
2024-02-16 09:23:23 +0000

    Comment out all console.logs in catch blocks

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -31,7 +31,7 @@ function PostsPage({ message, filter = "" }) {
             setPosts(data);
             setHasLoaded(true);
         } catch(err){
-            console.log(err);
+            //console.log(err);
         }
     };
     setHasLoaded(false);
